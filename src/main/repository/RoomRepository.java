56a68502ed12f48c5ec5b1a45cee410cd478bdf2
donghoony <aru0504@naver.com>
2023-10-07 23:51:19 +0900

    refactor: initialize hashmap, remove idstrategy from param

--- src/main/repository/RoomRepository.java
+++ src/main/repository/RoomRepository.java
@@ -1,9 +1,9 @@
 package repository;
 
-import database.NumericIdEntityDataBase;
+import database.NumericIdEntityDatabase;
 import entity.Room;
 
-public class RoomRepository extends NumericIdEntityDataBase<Room> {
+public class RoomRepository extends NumericIdEntityDatabase<Room> {
 
     public RoomRepository() {
         super(Room.class, "theater.txt");
