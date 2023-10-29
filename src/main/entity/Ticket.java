20603ed6790387103a7720d69e251168bac2d26d
Jinwoo <59133874+jinwoo0302@users.noreply.github.com>
2023-10-29 17:25:33 +0900

    티켓 엔티티 및 티켓 서비스 수정 (#19)
    * Ticket 엔티티에 전화번호, 예매 취소 여부, 예매 시각, 취소 시각 추가하였습니다.
    TicketService에 예매 취소 메소드를 수정하였습니다.
    
    * 티켓서비스에서 전화번호를 입력받아 티켓 예매 내역 출력
    
    ---------
    
    Co-authored-by: ziheehan <gkswlgml90@gmail.com>

--- src/main/entity/Ticket.java
+++ src/main/entity/Ticket.java
@@ -1,14 +1,25 @@
 package entity;
 
+import java.time.LocalDateTime;
+
 public class Ticket extends Entity<String> {
 
     private final MovieSchedule movieSchedule;
     private final String seatId;
+    private final String phoneNumber;
+    private boolean isCanceled;
+    private final LocalDateTime reservationTime;
+    private LocalDateTime cancellationTime;
 
-    public Ticket(String Id, MovieSchedule movieSchedule, String seatId) {
+
+    public Ticket(String Id, MovieSchedule movieSchedule, String seatId, String phoneNumber, LocalDateTime reservationTime) {
         super(Id);
         this.movieSchedule = movieSchedule;
         this.seatId = seatId;
+        this.phoneNumber=phoneNumber;
+        this.isCanceled=false; //1이면 취소된 경우, 생성할 땐 0
+        this.reservationTime=reservationTime;
+        this.cancellationTime=null;
     }
 
     @Override
@@ -22,5 +33,11 @@ public class Ticket extends Entity<String> {
 
     public String getSeatId() {
         return seatId;
+    }
+
+    public String getPhoneNumber(){ return  phoneNumber;}
+    public void setIscanceled(LocalDateTime cancellationTime){
+        this.isCanceled=true;
+        this.cancellationTime=cancellationTime;
     }
 }
